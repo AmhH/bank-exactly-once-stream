b5d9258d28758fc77eebb1b461a5d862c3a58f81
Amh <abaams78@gmail.com>
2020-07-13 15:15:18 -0500

    producer for test

---
+++ src/test/java/com/example/kafka/streams/BankTransactionsProducerTest.java
@@ -0,0 +1,34 @@
+package com.example.kafka.streams;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BankTransactionsProducerTest {
+
+    @Test
+    public void newRandomTransactionsTest(){
+        ProducerRecord<String, String> record = BankTransactionsProducer.newRandomTransaction("john");
+        String key = record.key();
+        String value = record.value();
+
+        assertEquals("john", key);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode node = mapper.readTree(value);
+            assertEquals("john", node.get("name").asText());
+            //assertTrue("Amount should be less than 100", node.get("amount").asInt() < 100);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(value);
+
+    }
+}
